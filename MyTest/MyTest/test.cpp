9a0e0f9e25f41e7bdc023d3f5c183ce3a373340d
Vanilla <Vanilla@DESKTOP-SVRSQ5R>
2020-10-16 16:11:52 +0800

    提交

--- MyTest/MyTest/test.cpp
+++ MyTest/MyTest/test.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 #include "test.h"
+#include <vector>
+#include <string>   
+#include <cassert>
 int main() 
 {
-    test01();
+    int a = 5;
+    int b = 6;
+  //  test01();
+    int ret =  test02(a);
+    cout << ret << endl;
 
+
+    test03(&a, &b);
+    cout << a << b << endl;
+    /*vector<int> v;
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);*/
+
+    int arr[4] = { 0,1,2,3 };
+
+    test04(begin(arr),end(arr));
+    
+    /*int val = 0;
+    assert(val);*/
 }
--- MyTest/MyTest/test.h
+++ MyTest/MyTest/test.h
@@ -3,29 +3,45 @@
 #include <iostream>
 using namespace std;
 
-void test01() 
-{
+//void test01() 
+//{
 	/*int arr[4] = { 0,1,2,3 };
-	int* p = arr;
+//	int* p = arr;
+//
+//	constexpr size_t sz = sizeof(arr) / sizeof(*arr);
+//
+//	cout << sz<<endl;*/    //求数组中元素个数
+//
+//	double b = 3.41;
+//	int a = 2;
+//	double c = static_cast<double>(b) / a;
+//	cout << c << endl;
+//
+//	double d = 1.12312312;
+//	void* p = &d;
+//	double *dp = static_cast<double*>(p);
+//
+//	cout << *dp << dp << endl;
+//
+//}
 
-	constexpr size_t sz = sizeof(arr) / sizeof(*arr);
-
-	cout << sz<<endl;*/    //求数组中元素个数
-
-	double b = 3.41;
-	int a = 2;
-	double c = static_cast<double>(b) / a;
-	cout << c << endl;
-
-	double d = 1.12312312;
-	void* p = &d;
-	double *dp = static_cast<double*>(p);
-
-	cout << *dp << dp << endl;
-
-
-
+int test02(int val) 
+{
+	int ret = 1;
+	while (val > 1)
+	ret *= val--;
+	return ret;
 }
-
+void test03(int *p,int *q)
+{
+	int temp = *p;
+	*p = *q;
+	*q = temp;
+}
+void test04(const int *beg, const int *end)
+{
+	while (beg != end)
+		cout << *beg++ << endl;
+}
 
 #endif // TEST_H
